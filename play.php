b5559827e8d916bd909f8205bc5f9bea52762ee2
Cynthia <cynthialouis.dev@gmail.com>
2017-10-18 10:56:47 +0200

    remove web structure

--- play.php
+++ play.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/lib/Ship.php';
  * @param Ship $someShip
  */
 function printShipSummary($someShip) {
-    echo 'Ship name : '.$someShip->name;
+    echo 'Ship name : '.$someShip->getName();
     echo '<hr/>';
     $someShip->sayHello();
     echo '<hr/>';
@@ -18,19 +18,19 @@ function printShipSummary($someShip) {
 }
 
 $myShip = new Ship();
-$myShip->name = 'Jedi Starship';
-$myShip->weaponPower = 10;
+$myShip->setName('Jedi Starship');
+$myShip->setWeaponPower(10);
 
 $otherShip = new Ship();
-$otherShip->name = 'Imperial Shuttle';
-$otherShip->weaponPower = 5;
+$otherShip->setName('Imperial Shuttle');
+$otherShip->setWeaponPower(5);
 $otherShip->setStrength(50);
 
 printShipSummary($myShip);
 printShipSummary($otherShip);
 
 if ($myShip->doesGivenShipHaveMoreStrength($otherShip)) {
-    echo $otherShip->name.' has more strength';
+    echo $otherShip->getName().' has more strength';
 } else {
-    echo $myShip->name.' has more strength';
+    echo $myShip->getName().' has more strength';
 }
